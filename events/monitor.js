3a6d97abd9455d6edc6fa0a659c130a8bdfdddcc
Neel Verma <spookyneel@gmail.com>
2022-05-25 10:22:38 +0530

    changes to filter

--- events/monitor.js
+++ events/monitor.js
@@ -5,7 +5,7 @@ const blacklist = json('jsons/data.json').data.blacklist;
 module.exports = async (bot, message) => {
     if (message.author.bot) return;
     if (message.channel.id !== process.env['CHANNEL']) return;
-    const commands = ['!stats', '!lb'];
+    const commands = ['!stats', '!lb', '!deduct', '!delete'];
     if (commands.some(el => message.content.toLowerCase().includes(el))) return;
     if (!blacklist.includes(message.author.id)) {
         await addXP(message.author.id, message.guild.id, message.author.username, 1);
